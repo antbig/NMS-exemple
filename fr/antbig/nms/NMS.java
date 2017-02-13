0abdafd1bf0c10d814b5771a9a246e49f6ddcf56
Leonard Antoine <admin@antbig.fr>
2017-02-13 18:44:46 +0100

    Update NMS.java

--- fr/antbig/nms/NMS.java
+++ fr/antbig/nms/NMS.java
@@ -7,7 +7,7 @@ public class NMS {
 
     public static Utils getUtils() {
         String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
-        String name = "fr.Antbig.Neptune.bukkit.nms." + version + "NMS_Utils";
+        String name = "fr.antbig.nms." + version + "NMS_Utils";
         try {
             Class<?> nmsClass = Class.forName(name);
             return (Utils)(nmsClass).newInstance() ;
@@ -19,7 +19,7 @@ public class NMS {
 
     public static FakePlayer getFakePlayer() {
         String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
-        String name = "fr.Antbig.Neptune.bukkit.nms." + version + "NMS_FakePlayer";
+        String name = "fr.antbig.nms." + version + "NMS_FakePlayer";
         try {
             Class<?> nmsClass = Class.forName(name);
             return (FakePlayer)(nmsClass).newInstance() ;
